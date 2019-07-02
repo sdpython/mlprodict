1834f0c17d827dddcbb4e2ef1ab2de770cd62667
xavier dupré <xavier.dupre@gmail.com>
2019-07-02 15:38:50 +0200

    update notebook

--- _unittests/ut_documentation/test_run_notebooks_onnx_sbs.py
+++ _unittests/ut_documentation/test_run_notebooks_onnx_sbs.py
@@ -4,9 +4,13 @@
 """
 import os
 import unittest
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.utils.testing import ignore_warnings
 from pyquickhelper.loghelper import fLOG
+from pyquickhelper.texthelper.version_helper import compare_module_version
 from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
 from pyquickhelper.pycode import add_missing_development_version, ExtTestCase
+from skl2onnx import __version__ as skl2onnx_version
 import mlprodict
 
 
@@ -15,6 +19,9 @@ class TestFunctionTestNotebookOnnxSbs(ExtTestCase):
     def setUp(self):
         add_missing_development_version(["jyquickhelper"], __file__, hide=True)
 
+    @unittest.skipIf(compare_module_version(skl2onnx_version, "1.5.0") <= 0,
+                     reason="int64 not implemented for constants")
+    @ignore_warnings(category=(UserWarning, ConvergenceWarning, RuntimeWarning))
     def test_notebook_onnx_sbs(self):
         fLOG(
             __file__,
