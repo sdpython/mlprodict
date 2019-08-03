7eb840eedbce9d13c79873b45facbed60a4c13ee
xavier dupré <xavier.dupre@gmail.com>
2019-08-04 00:05:11 +0200

    refactor DimensionObject for shape object

--- _unittests/ut_onnxrt/test_shape_object.py
+++ _unittests/ut_onnxrt/test_shape_object.py
@@ -12,14 +12,21 @@ class TestShapeObject(ExtTestCase):
         i1 = DimensionObject(1)
         i2 = DimensionObject(3)
         i3 = i1 + i2
-        self.assertEqual(i3.evaluated(), 4)
-        self.assertEqual("DimensionObject(4)", repr(i3))
+        self.assertEqual(
+            "DimensionObject(ShapeOperatorAdd(DimensionObject(1), DimensionObject(3)))", repr(i3))
+        self.assertEqual(i3.to_string(), '4')
+        v = i3.evaluate()
+        self.assertEqual(v, 4)
 
         i1 = DimensionObject(1)
         i2 = DimensionObject("x")
         i3 = i1 + i2
-        self.assertEqual(i3.evaluated(), '1+x')
-        self.assertEqual("DimensionObject('1+x')", repr(i3))
+        self.assertEqual(i3.to_string(), '1+x')
+        self.assertEqual(
+            "DimensionObject(ShapeOperatorAdd(DimensionObject(1), DimensionObject('x')))", repr(i3))
+        v = i3.evaluate(x=1)
+        self.assertEqual(v, 2)
+        self.assertRaise(lambda: i3.evaluate(), NotImplementedError)
 
         self.assertRaise(lambda: DimensionObject((1, )) + 1, TypeError)
         self.assertRaise(lambda: DimensionObject(
@@ -29,14 +36,21 @@ class TestShapeObject(ExtTestCase):
         i1 = DimensionObject(2)
         i2 = DimensionObject(3)
         i3 = i1 * i2
-        self.assertEqual(i3.evaluated(), 6)
-        self.assertEqual("DimensionObject(6)", repr(i3))
+        self.assertEqual(
+            "DimensionObject(ShapeOperatorMul(DimensionObject(2), DimensionObject(3)))", repr(i3))
+        self.assertEqual(i3.to_string(), '6')
+        v = i3.evaluate()
+        self.assertEqual(v, 6)
 
         i1 = DimensionObject(2)
         i2 = DimensionObject("x")
         i3 = i1 * i2
-        self.assertEqual(i3.evaluated(), '2*x')
-        self.assertEqual("DimensionObject('2*x')", repr(i3))
+        self.assertEqual(i3.to_string(), '2*x')
+        self.assertEqual(
+            "DimensionObject(ShapeOperatorMul(DimensionObject(2), DimensionObject('x')))", repr(i3))
+        v = i3.evaluate(x=2)
+        self.assertEqual(v, 4)
+        self.assertRaise(lambda: i3.evaluate(), NotImplementedError)
 
         self.assertRaise(lambda: DimensionObject((1, )) * 1, TypeError)
         self.assertRaise(lambda: DimensionObject(
--- mlprodict/onnxrt/shape_object.py
+++ mlprodict/onnxrt/shape_object.py
@@ -4,6 +4,148 @@
 """
 
 
+class ShapeOperator:
+    """
+    Base class for all shapes operator.
+    """
+
+    def __init__(self, name, fct, fct_string, *args):
+        """
+        @param      name        display name of the operator
+        @param      fct         function doing the operator
+                                if argument are numeric
+        @param      fct_string  function represented as a string
+        @param      args        argument of the operator
+        """
+        self._name = name
+        self._fct = fct
+        self._fct_string = fct_string
+        self._args = args
+        for a in self._args:
+            if not isinstance(a, DimensionObject):
+                raise TypeError(
+                    "All arguments must be of type DimensionObject not '{}'.".format(type(a)))
+
+    def __repr__(self):
+        """
+        usual
+        """
+        return "{0}('{1}', {2}, '{2}', {3})".format(
+            self.__class__.__name__, self._name,
+            self._fct_string, self._args)
+
+    def to_string(self):
+        """
+        Displays as a string.
+
+        @return     a string
+        """
+        raise NotImplementedError(
+            "Operator '{}' does not implement 'to_string': {}.".format(
+                self.__class__.__name__, repr(self)))
+
+    def evaluate(self, **kwargs):
+        """
+        Evalutes the operator.
+
+        @param  kwargs      value for the variables.
+        @return             string or integer
+        """
+        args = []
+        for a in self._args:
+            a = DimensionObject._same_(a)
+            v = a.evaluate(**kwargs)
+            args.append(v)
+        try:
+            res = self._fct(*args)
+        except TypeError as e:
+            raise RuntimeError(
+                "Unable to evaluate operator {} due to {}".format(repr(self), e))
+        return res
+
+
+class ShapeBinaryOperator(ShapeOperator):
+    """
+    Base class for shape binary operator.
+    """
+
+    def __init__(self, name, fct, fct_string, x, y):
+        """
+        @param      name        display name of the operator
+        @param      fct         function doing the operator
+                                if argument are numeric
+        @param      fct_string  function represented as a string
+        @param      x           first argument
+        @param      y           second argument
+        """
+        ShapeOperator.__init__(self, name, fct, fct_string, x, y)
+        if isinstance(x, tuple):
+            raise TypeError('x cannot be a tuple')
+        if isinstance(y, tuple):
+            raise TypeError('y cannot be a tuple')
+
+    def to_string(self):
+        """
+        Applies binary operator to a dimension.
+
+        @return             a string
+        """
+        x, y = self._args  # pylint: disable=W0632
+        if isinstance(x._dim, int):
+            if isinstance(y._dim, int):
+                return DimensionObject(self._fct(x._dim, y._dim)).to_string()
+            if isinstance(y._dim, str):
+                return DimensionObject("{}{}{}".format(x._dim, self._name, y._dim)).to_string()
+            raise TypeError("Unable to handle type '{}'.".format(type(y._dim)))
+        elif isinstance(x._dim, str):
+            if isinstance(y._dim, int):
+                return DimensionObject("{}{}{}".format(x._dim, self._name, y._dim)).to_string()
+            elif isinstance(y._dim, str):
+                return DimensionObject("({}){}({})".format(x._dim, self._name, y._dim)).to_string()
+            raise TypeError("Unable to handle type '{}'.".format(type(y._dim)))
+        else:
+            raise TypeError(
+                "Unable to handle type '{}'.".format(type(x._dim)))
+
+
+class ShapeOperatorAdd(ShapeBinaryOperator):
+    """
+    Shape addition.
+    """
+
+    def __init__(self, x, y):
+        ShapeBinaryOperator.__init__(
+            self, '+', lambda a, b: a + b, 'lambda a, b: a + b', x, y)
+
+    def __repr__(self):
+        """
+        Displays a string.
+
+        @return             a string
+        """
+        return "{0}({1}, {2})".format(
+            self.__class__.__name__, repr(self._args[0]), repr(self._args[1]))
+
+
+class ShapeOperatorMul(ShapeBinaryOperator):
+    """
+    Shape multiplication.
+    """
+
+    def __init__(self, x, y):
+        ShapeBinaryOperator.__init__(
+            self, '*', lambda a, b: a * b, 'lambda a, b: a * b', x, y)
+
+    def __repr__(self):
+        """
+        Displays a string.
+
+        @return             a string
+        """
+        return "{0}({1}, {2})".format(
+            self.__class__.__name__, repr(self._args[0]), repr(self._args[1]))
+
+
 class DimensionObject:
     """
     One dimension of a shape.
@@ -18,6 +160,8 @@ class DimensionObject:
             self._dim = 'x'
         else:
             self._dim = obj
+        if isinstance(self._dim, tuple):
+            raise TypeError("obj cannot be a tuple")
 
     @property
     def dim(self):
@@ -32,6 +176,10 @@ class DimensionObject:
         """
         if isinstance(self._dim, int):
             return "DimensionObject({})".format(self._dim)
+        if isinstance(self._dim, DimensionObject):
+            return repr(self._dim)
+        if isinstance(self._dim, ShapeOperator):
+            return "DimensionObject({})".format(repr(self._dim))
         return "DimensionObject('{}')".format(self._dim)
 
     @staticmethod
@@ -44,57 +192,48 @@ class DimensionObject:
             return obj
         return DimensionObject(obj)
 
-    def evaluated(self, **kwargs):
+    def to_string(self):
+        """
+        Represents the dimension as a string.
+        """
+        if isinstance(self._dim, int):
+            return '{}'.format(self._dim)
+        if isinstance(self._dim, ShapeOperator):
+            return self._dim.to_string()
+        if isinstance(self._dim, str):
+            return self._dim
+        raise NotImplementedError(
+            "Not implemented for '{}'.".format(repr(self)))
+
+    def evaluate(self, **kwargs):
         """
         Evalutes the dimension.
 
         @param  kwargs      value for the variables.
+        @return             string or integer
         """
         if isinstance(self._dim, int):
             return self._dim
-        if isinstance(self._dim, str):
-            if len(kwargs) == 0:
-                return self._dim
+        if isinstance(self._dim, (ShapeOperator, DimensionObject)):
+            return self._dim.evaluate(**kwargs)
+        if isinstance(self._dim, str) and self._dim in kwargs:
+            return kwargs[self._dim]
         raise NotImplementedError(
             "Not implemented for '{}'.".format(repr(self)))
-
-    def _binaryop_(self, obj, strop, fct):
-        """
-        Applies binary operator to a dimension.
-
-        @param      obj     other @see cl DimensionObject
-        @param      strop   operator string
-        @param      fct     python function which applies the operator
-        @return             @see cl DimensionObject
-        """
-        o = DimensionObject._same_(obj)
-        if isinstance(self._dim, int):
-            if isinstance(o._dim, int):
-                return DimensionObject(fct(self._dim, o._dim))
-            if isinstance(o._dim, str):
-                return DimensionObject("{}{}{}".format(self._dim, strop, o._dim))
-            raise TypeError("Unable to handle type '{}'.".format(type(o._dim)))
-        elif isinstance(self._dim, str):
-            if isinstance(o._dim, int):
-                return DimensionObject("{}{}{}".format(self._dim, strop, o._dim))
-            elif isinstance(o._dim, str):
-                return DimensionObject("({}){}({})".format(self._dim, strop, o._dim))
-            raise TypeError("Unable to handle type '{}'.".format(type(o._dim)))
-        else:
-            raise TypeError(
-                "Unable to handle type '{}'.".format(type(self._dim)))
 
     def __add__(self, obj):
         """
         usual
         """
-        return self._binaryop_(obj, '+', lambda x, y: x + y)
+        return DimensionObject(
+            ShapeOperatorAdd(self, DimensionObject._same_(obj)))
 
     def __mul__(self, obj):
         """
         usual
         """
-        return self._binaryop_(obj, '*', lambda x, y: x * y)
+        return DimensionObject(
+            ShapeOperatorMul(self, DimensionObject._same_(obj)))
 
 
 class ShapeObject:
