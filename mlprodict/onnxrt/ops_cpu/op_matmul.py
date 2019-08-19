d6a83c0dfbe337f239e37e9da29c84c87dcd4607
xavier dupré <xavier.dupre@gmail.com>
2019-08-19 17:40:01 +0200

    fix inplace code for #33

--- mlprodict/onnxrt/ops_cpu/op_matmul.py
+++ mlprodict/onnxrt/ops_cpu/op_matmul.py
@@ -14,10 +14,18 @@ class MatMul(OpRunBinaryNum):
         OpRunBinaryNum.__init__(self, onnx_node, desc=desc, **options)
 
     def _run(self, a, b):  # pylint: disable=W0221
-        if b.shape[1] <= a.shape[1] and self.inplaces.get(0, False):
-            numpy.dot(a, b, out=a[:, :b.shape[1]])
-            return (a[:, :b.shape[1]], )
-        if a.shape[0] <= b.shape[0] and self.inplaces.get(1, False):
-            numpy.dot(a, b, out=b[:a.shape[0], :])
-            return (b[:a.shape[0], :], )
+        if self.inplaces.get(0, False):
+            if len(b.shape) == len(a.shape) == 2 and b.shape[0] <= a.shape[1]:
+                numpy.dot(a, b, out=a[:, :b.shape[1]])
+                return (a[:, :b.shape[1]], )
+            if len(b.shape) == 1:
+                numpy.dot(a, b, out=a[:, :1])
+                return (a[:, :1], )
+        if self.inplaces.get(1, False):
+            if len(b.shape) == len(a.shape) == 2 and b.shape[1] <= a.shape[0]:
+                numpy.dot(a, b, out=b[:a.shape[0], :])
+                return (b[:a.shape[0], :], )
+            if len(a.shape) == 1:
+                numpy.dot(a, b, out=b[:1, :])
+                return (b[:1, :], )
         return (numpy.dot(a, b), )
--- mlprodict/onnxrt/ops_cpu/op_mul.py
+++ mlprodict/onnxrt/ops_cpu/op_mul.py
@@ -14,10 +14,10 @@ class Mul(OpRunBinaryNum):
         OpRunBinaryNum.__init__(self, onnx_node, desc=desc, **options)
 
     def _run(self, a, b):  # pylint: disable=W0221
-        if self.inplaces.get(0, False):
+        if self.inplaces.get(0, False) and a.size >= b.size:
             numpy.multiply(a, b, out=a)
             return (a, )
-        if self.inplaces.get(1, False):
+        if self.inplaces.get(1, False) and a.size <= b.size:
             numpy.multiply(a, b, out=b)
             return (b, )
         return (numpy.multiply(a, b), )
