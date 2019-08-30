c25113feb100b763d16a1f0dd2f5f1fda089471c
xavier dupré <xavier.dupre@gmail.com>
2019-08-30 12:03:05 +0200

    supports multiple targets for knn

--- _unittests/ut_onnx_conv/test_onnx_conv_knn.py
+++ _unittests/ut_onnx_conv/test_onnx_conv_knn.py
@@ -25,29 +25,72 @@ class TestOnnxConvKNN(ExtTestCase):
             res = register_converters(True)
         self.assertGreater(len(res), 2)
 
-    def onnx_test_knn_single_regressor(self, dtype):
+    def onnx_test_knn_single_regressor(self, dtype, n_targets=1, debug=False, **kwargs):
         iris = load_iris()
         X, y = iris.data, iris.target
         y = y.astype(dtype)
+        if n_targets != 1:
+            yn = numpy.empty((y.shape[0], n_targets), dtype=dtype)
+            for i in range(n_targets):
+                yn[:, i] = y + i
+            y = yn
         X_train, X_test, y_train, _ = train_test_split(X, y, random_state=11)
         X_test = X_test.astype(dtype)
-        clr = KNeighborsRegressor()
+        clr = KNeighborsRegressor(**kwargs)
         clr.fit(X_train, y_train)
 
         model_def = to_onnx(clr, X_train.astype(dtype),
                             dtype=dtype, rewrite_ops=True)
         oinf = OnnxInference(model_def, runtime='python')
 
-        y = oinf.run({'X': X_test})
+        if debug:
+            y = oinf.run({'X': X_test}, verbose=1, fLOG=print)
+        else:
+            y = oinf.run({'X': X_test})
         self.assertEqual(list(sorted(y)), ['variable'])
         lexp = clr.predict(X_test)
         if dtype == numpy.float32:
-            self.assertEqualArray(lexp, y['variable'].ravel(), decimal=5)
+            self.assertEqualArray(lexp, y['variable'], decimal=5)
         else:
-            self.assertEqualArray(lexp, y['variable'].ravel())
+            self.assertEqualArray(lexp, y['variable'])
 
     def test_onnx_test_knn_single_regressor32(self):
         self.onnx_test_knn_single_regressor(numpy.float32)
+
+    def test_onnx_test_knn_single_regressor32_balltree(self):
+        self.onnx_test_knn_single_regressor(
+            numpy.float32, algorithm='ball_tree')
+
+    def test_onnx_test_knn_single_regressor32_kd_tree(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, algorithm='kd_tree')
+
+    def test_onnx_test_knn_single_regressor32_brute(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, algorithm='brute')
+
+    def test_onnx_test_knn_single_regressor64(self):
+        self.onnx_test_knn_single_regressor(numpy.float64)
+
+    def test_onnx_test_knn_single_regressor32_target2(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, n_targets=2)
+
+    def test_onnx_test_knn_single_regressor32_k1(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, n_neighbors=1)
+
+    def test_onnx_test_knn_single_regressor32_k1_target2(self):
+        self.onnx_test_knn_single_regressor(
+            numpy.float32, n_neighbors=1, n_targets=2)
+
+    def test_onnx_test_knn_single_regressor32_minkowski(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, metric='minkowski')
+
+    @unittest.skip(reason="not yet implemented")
+    def test_onnx_test_knn_single_regressor32_distance(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, weights='distance')
+
+    @unittest.skip(reason="not yet implemented")
+    def test_onnx_test_knn_single_regressor32_minkowski_p3(self):
+        self.onnx_test_knn_single_regressor(numpy.float32, metric='minkowski',
+                                            metric_params={'p': 3})
 
 
 if __name__ == "__main__":
--- mlprodict/onnx_conv/sklconv/knn.py
+++ mlprodict/onnx_conv/sklconv/knn.py
@@ -6,8 +6,9 @@
 import numpy
 from skl2onnx.algebra.complex_functions import onnx_cdist
 from skl2onnx.algebra.onnx_ops import (  # pylint: disable=E0611
-    OnnxTopK, OnnxMul, OnnxArrayFeatureExtractor, OnnxReduceMean,  # pylint: disable=E0611
-    OnnxFlatten, OnnxShape, OnnxReshape  # pylint: disable=E0611
+    OnnxTopK, OnnxMul, OnnxArrayFeatureExtractor, OnnxReduceMean,
+    OnnxFlatten, OnnxShape, OnnxReshape,
+    OnnxConcat, OnnxTranspose
 )
 
 
@@ -43,10 +44,7 @@ def convert_nearest_neighbors_regressor(scope, operator, container):
 
     single_reg = (len(op._y.shape) == 1 or len(
         op._y.shape) == 2 and op._y.shape[1] == 1)
-    if not single_reg:
-        raise NotImplementedError("Only single regression is implemented.")
-
-    # ndim = 1 if single_reg else op._y.shape[1]
+    ndim = 1 if single_reg else op._y.shape[1]
 
     metric = op.effective_metric_
     neighb = op._fit_X.astype(container.dtype)
@@ -60,9 +58,21 @@ def convert_nearest_neighbors_regressor(scope, operator, container):
         X, neighb, k, metric=metric, dtype=dtype, op_version=opv)
     shape = OnnxShape(top_indices, op_version=opv)
     flattened = OnnxFlatten(top_indices, op_version=opv)
+    if ndim > 1:
+        # shape = (ntargets, ) + shape
+        training_labels = training_labels.T
+        shape = OnnxConcat(numpy.array([ndim], dtype=numpy.int64),
+                           shape, op_version=opv)
+        axis = 2
+    else:
+        training_labels = training_labels.ravel()
+        axis = 1
     extracted = OnnxArrayFeatureExtractor(
-        training_labels.ravel(), flattened, op_version=opv)
+        training_labels, flattened, op_version=opv)
     reshaped = OnnxReshape(extracted, shape, op_version=opv)
-    res = OnnxReduceMean(reshaped, axes=[1], op_version=opv,
-                         output_names=out)
+    if ndim > 1:
+        reshaped = OnnxTranspose(reshaped, op_version=opv, perm=[1, 0, 2])
+
+    res = OnnxReduceMean(reshaped, axes=[axis], op_version=opv,
+                         keepdims=0, output_names=out)
     res.add_to(scope, container)
