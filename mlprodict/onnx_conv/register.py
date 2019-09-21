7dc268289fbc3fde38ed467977cd97e943c3f389
xavier dupré <xavier.dupre@gmail.com>
2019-09-21 20:14:14 +0200

    Fix alias name

--- mlprodict/onnx_conv/register.py
+++ mlprodict/onnx_conv/register.py
@@ -77,7 +77,7 @@ def _register_converters_lightgbm(exc=True):
     if LGBMClassifier is not None:
         from .shape_calculators.conv_lightgbm import calculate_linear_classifier_output_shapes
         from .operator_converters.conv_lightgbm import convert_lightgbm
-        update_registered_converter(LGBMClassifier, 'LightGbmLGBMClassifier',
+        update_registered_converter(LGBMClassifier, 'LgbmClassifier',
                                     calculate_linear_classifier_output_shapes,
                                     convert_lightgbm, parser=_custom_parser_lightgbm)
         registered.append(LGBMClassifier)
@@ -120,7 +120,7 @@ def _register_converters_xgboost(exc=True):
             raise e
         else:
             warnings.warn(
-                "Cannot register LGBMClassifier due to '{}'.".format(e))
+                "Cannot register XGBClassifier due to '{}'.".format(e))
             XGBClassifier = None
     if XGBClassifier is not None:
         from skl2onnx.common.shape_calculator import calculate_linear_classifier_output_shapes
