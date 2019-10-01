c37e3f0791451c622fbb518b81af651b369c4c64
xavier dupré <xavier.dupre@gmail.com>
2019-10-01 23:21:02 +0200

    Update run_asv.sh

--- run_asv.sh
+++ run_asv.sh
@@ -1,6 +1,9 @@
+echo --BENCH--
 python3.7 -m asv run --show-stderr --config mlprodict/asv_benchmark/asv.conf.json
 if [ -d dist/html/asv ]
 then
+    echo --REMOVE HTML--
     rm dist/html/asv -r -f
 fi
+echo --PUBLISH--
 python3.7 -m asv publish --config mlprodict/asv_benchmark/asv.conf.json -o dist/html/asv || exit 1
