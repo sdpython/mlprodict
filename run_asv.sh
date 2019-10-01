4abea8b01757dad75ac103a31725e8fc9132868e
xavier dupré <xavier.dupre@gmail.com>
2019-10-01 23:08:31 +0200

    update CI

--- run_asv.sh
+++ run_asv.sh
@@ -1,1 +1,6 @@
 python3.7 -m asv run --show-stderr --config mlprodict/asv_benchmark/asv.conf.json
+if [ -d dist/html/asv ]
+then
+    rm dist/html/asv -r -f
+fi
+python3.7 -m asv publish --config mlprodict/asv_benchmark/asv.conf.json -o dist/html/asv || exit 1
