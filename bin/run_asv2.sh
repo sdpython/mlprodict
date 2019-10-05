b92f5668bdc538eaa0f105584aa616cd982d0d4b
xavier dupré <xavier.dupre@gmail.com>
2019-10-05 10:02:35 +0200

    rename files

--- bin/run_asv2.sh
+++ bin/run_asv2.sh
@@ -1,9 +1,9 @@
 echo --BENCH--
-python3.7 -m asv run --show-stderr --config benches/asv.conf.json
+python3.7 -m asv run --show-stderr --config _benches/asv.conf.json
 if [ -d build/html ]
 then
     echo --REMOVE HTML--
     rm build/html -r -f
 fi
 echo --PUBLISH--
-python3.7 -m asv publish --config benches/asv.conf.json -o ../build/html || exit 1
+python3.7 -m asv publish --config _benches/asv.conf.json -o ../build/html || exit 1
