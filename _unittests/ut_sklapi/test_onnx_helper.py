1bf8ea645620294b91816c1990231d0c676d661d
xavier dupré <xavier.dupre@gmail.com>
2019-11-15 16:13:48 +0100

    Fix pep8.

--- _unittests/ut_sklapi/test_onnx_helper.py
+++ _unittests/ut_sklapi/test_onnx_helper.py
@@ -54,8 +54,10 @@ class TestOnnxHelper(ExtTestCase):
         self.assertEqual(X2.shape, (2, 2))
 
     def test_onnx_helper_load_save_init(self):
-        model = make_pipeline(Binarizer(), OneHotEncoder(
-            sparse=False), StandardScaler())
+        model = make_pipeline(
+            Binarizer(),
+            OneHotEncoder(sparse=False, handle_unknown='ignore'),
+            StandardScaler())
         X = numpy.array([[0.1, 1.1], [0.2, 2.2], [0.4, 2.2], [0.2, 2.4]])
         model.fit(X)
         model_onnx = convert_sklearn(
@@ -66,7 +68,6 @@ class TestOnnxHelper(ExtTestCase):
         list(enumerate_model_node_outputs(model))
         new_model = select_model_inputs_outputs(model, 'variable')
         self.assertTrue(new_model.graph is not None)  # pylint: disable=E1101
-
         tr1 = self.get_model(model)
         tr2 = self.get_model(new_model)
         X = X.astype(numpy.float32)
