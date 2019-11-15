ba4ea3ff896c84c30c0db2c9fa65e6408b2eff84
xavier dupré <xavier.dupre@gmail.com>
2019-11-15 14:59:34 +0100

    Update run_asv2.bat

--- bin/run_asv2.bat
+++ bin/run_asv2.bat
@@ -1,13 +1,14 @@
 @echo off
 set current=%~dp0
 cd %~dp0..\_benches
+rem goto publish:
 @echo --CREATE-BENCH--
-python -m mlprodict asv_bench --location . --models "LinearRegression,LogisticRegression" --build "../build"
-exit
+python -m mlprodict asv_bench --location . --models "LinearRegression" --build "../build"
 @echo --RUN-BENCH--
-python -m asv run --show-stderr --config=asv.conf.json
-if exist ..\build\html rm ..\build\html -r -f
+python -m asv run --show-stderr --config=asv.conf.json --environment existing:same
+:publish:
 @echo --PUBLISH--
+if exist ..\build\html rmdir ..\build\html -r -f
 python -m asv publish -o ..\build\html
 @echo --END--
 set %current%
