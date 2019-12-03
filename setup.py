90a9c92653b9836609c34d418bef44938ae5fdd1
xavier dupré <xavier.dupre@gmail.com>
2019-12-03 16:15:34 +0100

    Fixes #30, enable openmp on mac osx

--- setup.py
+++ setup.py
@@ -201,9 +201,10 @@ if not r:
     elif sys.platform.startswith("darwin"):
         libraries_thread = None
         extra_compile_args = ['-stdlib=libc++', '-mmacosx-version-min=10.7',
-                              '-fpermissive', '-std=c++11']  # , '-openmp']
-        extra_link_args = None  # ['-lomp']
-        define_macros = None  # [('USE_OPENMP', None)]
+                              '-fpermissive', '-std=c++11', 
+                              '-Xpreprocessor', '-fopenmp']
+        extra_link_args = ['-lomp']
+        define_macros = [('USE_OPENMP', None)]
     else:
         libraries_thread = None
         # , '-o2', '-mavx512f']
