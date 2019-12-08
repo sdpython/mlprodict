a3786b9441a67adf0beea8d5e6d506a1cee4075e
xavier dupré <xavier.dupre@gmail.com>
2019-12-08 21:43:55 +0100

    Update run_asv2.sh

--- bin/run_asv2.sh
+++ bin/run_asv2.sh
@@ -3,7 +3,8 @@ python -m pip install --no-cache-dir --no-deps --index http://localhost:8067/sim
 echo --BENCH-CREATE--
 python -m mlprodict asv_bench --env same --location _benches --models "LinearRegression,LogisticRegression" --build "../build" || exit 1
 echo --BENCH-RUN--
-python -m asv run --show-stderr --config _benches/asv.conf.json --environment same --python same
+# python -m asv run --show-stderr --config _benches/asv.conf.json --environment same --python same
+python -m asv run --show-stderr --config _benches/asv.conf.json
 if [ -d build/html ]
 then
     echo --REMOVE HTML--
