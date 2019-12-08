1b6a54b2df99082decbdcce8442c435c6ca34c13
xavier dupré <xavier.dupre@gmail.com>
2019-12-08 19:30:07 +0100

    update scripts

--- bin/run_asv.sh
+++ bin/run_asv.sh
@@ -1,9 +1,9 @@
 echo --BENCH--
-python3.7 -m asv run --show-stderr --config mlprodict/asv_benchmark/asv.conf.json
+python -m asv run --show-stderr --config mlprodict/asv_benchmark/asv.conf.json
 if [ -d dist/html/asv ]
 then
     echo --REMOVE HTML--
     rm dist/html/asv -r -f
 fi
 echo --PUBLISH--
-python3.7 -m asv publish --config mlprodict/asv_benchmark/asv.conf.json -o ../../dist/asv/html || exit 1
+python -m asv publish --config mlprodict/asv_benchmark/asv.conf.json -o ../../dist/asv/html || exit 1
--- bin/run_asv2.sh
+++ bin/run_asv2.sh
@@ -1,11 +1,13 @@
+echo --INSTALL--
+python -m pip install --no-cache-dir --no-deps --index http://localhost:8067/simple/ mlprodict --extra-index-url=https://pypi.python.org/simple/
 echo --BENCH-CREATE--
-python3.7 -m mlprodict asv_bench --env same --location _benches --models "LinearRegression,LogisticRegression" --build "../build" || exit 1
+python -m mlprodict asv_bench --env same --location _benches --models "LinearRegression,LogisticRegression" --build "../build" || exit 1
 echo --BENCH-RUN--
-python3.7 -m asv run --show-stderr --config _benches/asv.conf.json --environment same --python same
+python -m asv run --show-stderr --config _benches/asv.conf.json --environment same --python same
 if [ -d build/html ]
 then
     echo --REMOVE HTML--
     rm build/html -r -f
 fi
 echo --PUBLISH--
-python3.7 -m asv publish --config _benches/asv.conf.json -o ../build/html || exit 1
+python -m asv publish --config _benches/asv.conf.json -o ../build/html || exit 1
