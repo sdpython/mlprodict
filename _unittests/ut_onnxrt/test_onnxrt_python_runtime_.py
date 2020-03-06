d28190422d68bb4eaf0275374246d3a36f8d50d1
xavier dupré <xavier.dupre@gmail.com>
2020-03-06 14:37:11 +0100

    pep8

--- _unittests/ut_onnxrt/test_onnxrt_python_runtime_.py
+++ _unittests/ut_onnxrt/test_onnxrt_python_runtime_.py
@@ -1367,22 +1367,22 @@ class TestOnnxrtPythonRuntime(ExtTestCase):
         values = [1.1, 2.2, 3.3, 4.4, 5.5]
         values_tensor = make_tensor(
             name='test',
-            data_type=TensorProto.FLOAT,
+            data_type=TensorProto.FLOAT,  # pylint: disable=E1101
             dims=(5, ),
             vals=values
         )
         indices = [1, 3, 5, 7, 9]
         indices_tensor = make_tensor(
             name='test_indices',
-            data_type=TensorProto.INT64,
+            data_type=TensorProto.INT64,  # pylint: disable=E1101
             dims=(5, ),
             vals=indices
         )
         dense_shape = [10]
         sparse = make_sparse_tensor(values_tensor, indices_tensor, dense_shape)
-        self.assertEqual(sparse.values, values_tensor)
-        self.assertEqual(sparse.indices, indices_tensor)
-        self.assertEqual(sparse.dims, dense_shape)
+        self.assertEqual(sparse.values, values_tensor)  # pylint: disable=E1101
+        self.assertEqual(sparse.indices, indices_tensor)  # pylint: disable=E1101
+        self.assertEqual(sparse.dims, dense_shape)  # pylint: disable=E1101
 
         X = numpy.array([0.1, 0.2], dtype=numpy.float32)
         cst = OnnxConstant(value_floats=X)
