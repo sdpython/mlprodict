05b199bc9f6ed9e1c664512d615b84f12626f055
xavier dupré <xavier.dupre@gmail.com>
2020-03-26 16:21:38 +0100

    fix scenarios

--- _unittests/ut_onnx_conv/test_rt_valid_knn.py
+++ _unittests/ut_onnx_conv/test_rt_valid_knn.py
@@ -14,15 +14,12 @@ from mlprodict.onnxrt.validate.validate import RuntimeBadResultsError
 
 class TestRtValidateKNN(ExtTestCase):
 
-    @unittest_require_at_least(skl2onnx, '1.5.9999')
-    @skipif_circleci("no end")
-    def test_rt_knn_regressor(self):
+    def common_test_rt_knn_regressor(self, prob='b-reg', classifier=False, debug=True):
         fLOG(__file__, self._testMethodName, OutputPrint=__name__ == "__main__")
         logger = getLogger('skl2onnx')
         logger.disabled = True
         verbose = 2 if __name__ == "__main__" else 0
 
-        debug = True
         buffer = []
 
         def myprint(*args, **kwargs):
@@ -31,13 +28,16 @@ class TestRtValidateKNN(ExtTestCase):
         def fil(m, p, s, e, o):
             if o == [{}] or o == {}:  # pylint: disable=R1714
                 return False
-            if "-64" not in p and 'b-reg' in p:
+            if "kdt_" in s:
+                return False
+            if "-64" not in p and prob in p:
                 return True
             return False
 
+        model = "KNeighborsClassifier" if classifier else "KNeighborsRegressor"
         try:
             rows = list(enumerate_validated_operator_opsets(
-                verbose, models={"KNeighborsRegressor"},
+                verbose, models={model},
                 fLOG=myprint, fail_bad_results=False,
                 runtime='python', debug=debug, extended_list=True,
                 store_models=True, opset_min=11, opset_max=11,
@@ -57,9 +57,27 @@ class TestRtValidateKNN(ExtTestCase):
             got = oinf.run({'X': f2}, verbose=3, fLOG=print)
             exp = model.predict(f1.astype(numpy.float32))
             rows = [str(f1), str(f2), str(got), str(exp)]
-            raise RuntimeError("\n".join(rows)) from e
+            raise RuntimeError("\n-----\n".join(rows)) from e
+        except RuntimeError:
+            list(enumerate_validated_operator_opsets(
+                1, models={model},
+                fLOG=print, fail_bad_results=False,
+                runtime='python', debug=debug, extended_list=True,
+                store_models=True, opset_min=11, opset_max=11,
+                filter_scenario=lambda m, p, s, e, o: fil(m, p, s, e, o)))
         self.assertGreater(len(rows), 1)
         self.assertGreater(len(buffer), 1 if debug else 0)
+
+    @unittest_require_at_least(skl2onnx, '1.5.9999')
+    @skipif_circleci("no end")
+    def test_rt_knn_regressor_breg(self):
+        self.common_test_rt_knn_regressor()
+
+    @unittest_require_at_least(skl2onnx, '1.5.9999')
+    @skipif_circleci("no end")
+    def test_rt_knn_classifier_mlabel(self):
+        self.common_test_rt_knn_regressor(
+            prob='m-label', classifier=True, debug=False)
 
 
 if __name__ == "__main__":
