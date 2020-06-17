8e9c68e7bc25ca8fc29260a4ed6a457c3a19347c
xavier dupré <xavier.dupre@gmail.com>
2020-06-17 10:32:25 +0200

    check decision_path on random forest

--- _unittests/ut_onnx_conv/test_onnxrt_runtime_randomforest_decision_path.py
+++ _unittests/ut_onnx_conv/test_onnxrt_runtime_randomforest_decision_path.py
@@ -5,6 +5,7 @@ import unittest
 from logging import getLogger
 import numpy
 from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from pyquickhelper.pycode import ExtTestCase
 from mlprodict.onnxrt import OnnxInference
@@ -50,6 +51,40 @@ class TestOnnxrtRuntimeRandomForestDecisionPath(ExtTestCase):
         dec = model.decision_path(X)
         exp = binary_array_to_string(dec.todense())
         self.assertEqual(exp, res['decision_path'].ravel().tolist())
+
+    def test_randomforestregressor_decision_path(self):
+        model = RandomForestRegressor(max_depth=2, n_estimators=2)
+        X, y = make_classification(10, n_features=4, random_state=42)
+        X = X[:, :2].astype(numpy.float32)
+        model.fit(X, y)
+        model_onnx = to_onnx(
+            model, X, options={id(model): {'decision_path': True}})
+        sess = OnnxInference(model_onnx)
+        res = sess.run({'X': X})
+        pred = model.predict(X)
+        self.assertEqualArray(pred, res['variable'].ravel())
+        dec = model.decision_path(X)
+        exp = binary_array_to_string(dec[0].todense())
+        got = numpy.array([''.join(row) for row in res['decision_path']])
+        self.assertEqual(exp, got.tolist())
+
+    def test_randomforestclassifier_decision_path(self):
+        model = RandomForestClassifier(max_depth=2, n_estimators=2)
+        X, y = make_classification(10, n_features=4, random_state=42)
+        X = X[:, :2].astype(numpy.float32)
+        model.fit(X, y)
+        model_onnx = to_onnx(
+            model, X, options={id(model): {'decision_path': True, 'zipmap': False}})
+        sess = OnnxInference(model_onnx)
+        res = sess.run({'X': X})
+        pred = model.predict(X)
+        self.assertEqualArray(pred, res['label'].ravel())
+        prob = model.predict_proba(X)
+        self.assertEqualArray(prob, res['probabilities'])
+        dec = model.decision_path(X)
+        exp = binary_array_to_string(dec[0].todense())
+        got = numpy.array([''.join(row) for row in res['decision_path']])
+        self.assertEqual(exp, got.tolist())
 
 
 if __name__ == "__main__":
