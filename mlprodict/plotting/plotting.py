1f908a01505ef2d016ac9e68d0340f304d43b0b9
xavier dupré <sdpython@users.noreply.github.com>
2020-12-09 15:59:14 +0100

    Fixes #197, add function to plot onnx graph with matplotlib (#198)

--- mlprodict/plotting/plotting.py
+++ mlprodict/plotting/plotting.py
@@ -5,3 +5,4 @@
 
 from .plotting_benchmark import plot_benchmark_metrics  # pylint: disable=W0611
 from .plotting_validate_graph import plot_validate_benchmark  # pylint: disable=W0611
+from .plotting_onnx import plot_onnx  # pylint: disable=W0611
