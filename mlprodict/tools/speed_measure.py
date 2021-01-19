87c457c76405870f803e9288e71e637531ab8492
xavier dupré <xavier.dupre@gmail.com>
2021-01-19 01:44:41 +0100

    documentation

--- mlprodict/tools/speed_measure.py
+++ mlprodict/tools/speed_measure.py
@@ -21,7 +21,7 @@ def measure_time(stmt, context, repeat=10, number=50, div_by_number=False):
     .. runpython::
         :showcode:
 
-        from cpyquickhelper.numbers import measure_time
+        from mlprodict.tools import measure_time
         from math import cos
 
         res = measure_time("cos(x)", context=dict(cos=cos, x=5.))
