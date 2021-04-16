9cad628b43696e067e453c33f014f2f724caa3e8
xavier dupré <xavier.dupre@gmail.com>
2021-04-16 11:22:06 +0200

    Supports any embedded estimator with numpy API

--- mlprodict/npy/numpy_onnx_impl_skl.py
+++ mlprodict/npy/numpy_onnx_impl_skl.py
@@ -35,3 +35,35 @@ def logistic_regression(x, *, model=None):
     """
     return MultiOnnxVar(model, x, op=OnnxSubEstimator,
                         options={'zipmap': False})
+
+
+def classifier(x, *, model=None):
+    """
+    Returns any classifier from :epkg:`scikit-learn`
+    converted into ONNX assuming a converter is registered
+    with :epkg:`sklearn-onnx`. Option *zipmap* is set to false.
+
+    :param x: array, variable name, instance of :class:`OnnxVar
+        <mlprodict.npy.onnx_variable.OnnxVar>`
+    :param model: instance of :epkg:`sklearn:linear_model:LinearRegression`
+    :return: instance of :class:`MultiOnnxVar
+        <mlprodict.npy.onnx_variable.MultiOnnxVar>`, first
+        output is labels, second one is the probabilities
+    """
+    return MultiOnnxVar(model, x, op=OnnxSubEstimator,
+                        options={'zipmap': False})
+
+
+def regressor(x, *, model=None):
+    """
+    Returns any regressor from :epkg:`scikit-learn`
+    converted into ONNX assuming a converter is registered
+    with :epkg:`sklearn-onnx`.
+
+    :param x: array, variable name, instance of :class:`OnnxVar
+        <mlprodict.npy.onnx_variable.OnnxVar>`
+    :param model: instance of :epkg:`sklearn:linear_model:LinearRegression`
+    :return: instance of :class:`OnnxVar
+        <mlprodict.npy.onnx_variable.OnnxVar>`
+    """
+    return OnnxVar(model, x, op=OnnxSubEstimator)
