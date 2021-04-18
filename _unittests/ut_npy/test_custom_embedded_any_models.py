b11facb1f4ae8db396623a4a861fe7ce247ec870
xavier dupré <xavier.dupre@gmail.com>
2021-04-18 18:09:30 +0200

    Fixes #256, add method to validate input data in numpy API for ONNX

--- _unittests/ut_npy/test_custom_embedded_any_models.py
+++ _unittests/ut_npy/test_custom_embedded_any_models.py
@@ -91,6 +91,39 @@ class AnyCustomClusterOnnx(ClusterMixin, BaseEstimator):
 
 
 @onnxsklearn_class("onnx_graph")
+class AnyCustomClusterOnnxValid(ClusterMixin, BaseEstimator):
+
+    def __init__(self, base_estimator):
+        ClusterMixin.__init__(self)
+        BaseEstimator.__init__(self)
+        self.base_estimator = base_estimator
+
+    def fit(self, X, y, sample_weights=None):
+        if sample_weights is not None:
+            raise NotImplementedError(
+                "weighted sample not implemented in this example.")
+
+        self.estimator_ = self.base_estimator.fit(  # pylint: disable=W0201
+            X, y, sample_weights)
+        return self
+
+    def _validate_onnx_data(self, X):
+        if X.dtype not in (numpy.float32, numpy.float64):
+            raise ValueError(
+                "Input X must have dtype float32 or float64.")
+        X = BaseEstimator._validate_data(
+            self, X, reset=False, dtype=[numpy.float64, numpy.float32],
+            order='C')
+        return X
+
+    def onnx_graph(self, X):
+        res_model = nxnpskl.cluster(X, model=self.estimator_)
+        label = res_model[0].copy()
+        prob = res_model[1].copy()
+        return MultiOnnxVar(label, prob)
+
+
+@onnxsklearn_class("onnx_graph")
 class AnyCustomTransformerOnnx(TransformerMixin, BaseEstimator):
 
     def __init__(self, base_estimator):
@@ -214,6 +247,26 @@ class TestCustomEmbeddedModels(ExtTestCase):
     def test_function_transformer_embedded_float64(self):
         self.common_test_function_transformer_embedded(
             numpy.float64, StandardScaler())
+
+    @ignore_warnings((DeprecationWarning, RuntimeWarning))
+    def test_function_cluster_embedded_validation(self):
+        est = KMeans(2)
+        dtype = numpy.float32
+        X = numpy.random.randn(20, 2).astype(dtype)
+        y = ((X.sum(axis=1) + numpy.random.randn(
+             X.shape[0]).astype(numpy.float32)) >= 0).astype(numpy.int64)
+        dec = AnyCustomClusterOnnxValid(est)
+        dec.fit(X, y)
+        onx = to_onnx(dec, X.astype(dtype))
+        oinf = OnnxInference(onx)
+        exp = dec.predict(X)  # pylint: disable=E1101
+        prob = dec.transform(X)  # pylint: disable=E1101
+        got = oinf.run({'X': X})
+        self.assertEqual(dtype, prob.dtype)
+        self.assertEqualArray(exp, got['label'].ravel())
+        self.assertEqualArray(prob, got['scores'])
+        self.assertRaise(
+            lambda: dec.predict(X.astype(numpy.int64)), ValueError)
 
 
 if __name__ == "__main__":
