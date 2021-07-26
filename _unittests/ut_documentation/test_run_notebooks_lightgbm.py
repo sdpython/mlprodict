4c9a13066306853844f452f9e2f12c18f78ae03a
xavier dupré <xavier.dupre@gmail.com>
2021-07-26 16:39:00 +0200

    Adds notebook to check discrepencies with lightgbm

---
+++ _unittests/ut_documentation/test_run_notebooks_lightgbm.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@brief      test log(time=20s)
+"""
+import os
+import unittest
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
+from pyquickhelper.pycode import (
+    add_missing_development_version, ExtTestCase)
+import mlprodict
+
+
+class TestNotebookLightgbm(ExtTestCase):
+
+    def setUp(self):
+        add_missing_development_version(["jyquickhelper"], __file__, hide=True)
+
+    def test_notebook_lightgbm(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.assertNotEmpty(mlprodict is not None)
+        folder = os.path.join(os.path.dirname(__file__),
+                              "..", "..", "_doc", "notebooks")
+        test_notebook_execution_coverage(__file__, "lightgbm", folder,
+                                         this_module_name="mlprodict", fLOG=fLOG)
+
+
+if __name__ == "__main__":
+    unittest.main()
