ca2f5807e567bb5ec7809527560be991e66ca3d8
Xavier Dupré <xadupre@users.noreply.github.com>
2021-11-10 00:33:58 +0100

    Supports operator SequenceAt in OnnxInference (#319)
    * Supports operator SequenceAt in OnnxInference
    * Update shape_object.py

--- mlprodict/onnxrt/ops_cpu/op_sequence_at.py
+++ mlprodict/onnxrt/ops_cpu/op_sequence_at.py
@@ -4,28 +4,28 @@
 @file
 @brief Runtime operator.
 
-.. versionadded:: 0.7
+.. versionadded:: 0.8
 """
 from ._op import OpRun
 from ..shape_object import ShapeObject
 
 
-class SequenceConstruct(OpRun):
+class SequenceAt(OpRun):
 
     atts = {}
 
     def __init__(self, onnx_node, desc=None, **options):
         OpRun.__init__(self, onnx_node, desc=desc,
-                       atts=SequenceConstruct.atts, **options)
+                       atts=SequenceAt.atts, **options)
 
-    def _run(self, *data):  # pylint: disable=W0221
-        return (data, )
+    def _run(self, seq, index):  # pylint: disable=W0221
+        return (seq[index], )
 
-    def _infer_shapes(self, *data):  # pylint: disable=W0221
-        return (ShapeObject(None, dtype="sequence", subtype=data[0]), )
+    def _infer_shapes(self, seq, index):  # pylint: disable=W0221
+        return (ShapeObject(None, dtype=seq.subtype.dtype), )
 
     def _infer_types(self, *data):  # pylint: disable=W0221
-        return (list, )
+        return (None, )
 
     def _infer_sizes(self, *args):  # pylint: disable=W0221
         res = self.run(*args)
