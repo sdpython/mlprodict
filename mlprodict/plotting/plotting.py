d10650ba230c40ab6517137410a6920352a2993d
Xavier Dupré <xadupre@users.noreply.github.com>
2021-12-08 14:44:11 +0100

    Implements a simple text display for ONNX graph (#325)
    * add function to print out an onnx graph in text
    * implementation still buggy fut almost final
    * fix text display
    * fix import
    * Update config.yml

--- mlprodict/plotting/plotting.py
+++ mlprodict/plotting/plotting.py
@@ -1,9 +1,10 @@
+# pylint: disable=W0611
 """
 @file
 @brief Shorcuts to plotting functions.
 """
 
-from .text_plot import onnx_text_plot, onnx_text_plot_tree  # pylint: disable=W0611
-from .plotting_benchmark import plot_benchmark_metrics  # pylint: disable=W0611
-from .plotting_validate_graph import plot_validate_benchmark  # pylint: disable=W0611
-from .plotting_onnx import plot_onnx  # pylint: disable=W0611
+from .text_plot import onnx_text_plot, onnx_text_plot_tree, onnx_simple_text_plot
+from .plotting_benchmark import plot_benchmark_metrics
+from .plotting_validate_graph import plot_validate_benchmark
+from .plotting_onnx import plot_onnx
