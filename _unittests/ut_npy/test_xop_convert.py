688cfbc156a006bf158e0a450a5ff97553a7e872
xavier dupré <xavier.dupre@gmail.com>
2022-02-22 11:42:59 +0100

    finalize to_onnx

--- _unittests/ut_npy/test_xop_convert.py
+++ _unittests/ut_npy/test_xop_convert.py
@@ -5,9 +5,12 @@
 import unittest
 import numpy
 from pyquickhelper.pycode import ExtTestCase
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
 from mlprodict.onnxrt import OnnxInference
 from mlprodict.npy.xop import loadop
-from mlprodict.npy.xop_convert import OnnxSubOnnx
+from mlprodict.npy.xop_convert import OnnxSubOnnx, OnnxSubEstimator
+from mlprodict.npy.xop_variable import max_supported_opset
 
 
 class TestXOpsConvert(ExtTestCase):
@@ -53,6 +56,45 @@ class TestXOpsConvert(ExtTestCase):
         x = numpy.array([-2.4, 2.4], dtype=numpy.float32)
         got = oinf.run({'X': x})
         self.assertEqualArray(x.astype(numpy.int64), got['Y'])
+
+    def test_onnx_lr(self):
+        X, y = make_regression(n_features=2)  # pylint: disable=W0632
+        lr = LinearRegression()
+        lr.fit(X, y)
+        X32 = X.astype(numpy.float32)
+
+        OnnxIdentity, OnnxReshape = loadop("Identity", "Reshape")
+        ov = OnnxIdentity('X')
+        self.assertRaise(lambda: OnnxSubEstimator(lr, ov), NotImplementedError)
+        sub = OnnxSubEstimator(
+            lr, ov, op_version=max_supported_opset(),
+            initial_types=X32[:1])
+        r = repr(sub)
+        self.assertStartsWith('OnnxSubEstimator(LinearRegression()', r)
+        last = OnnxReshape(sub, numpy.array([-1], dtype=numpy.int64),
+                           output_names=['Y'])
+        onx = last.to_onnx(numpy.float32, numpy.float32, verbose=0)
+
+        oinf = OnnxInference(onx)
+        got = oinf.run({'X': X32})
+        expected = lr.predict(X32)
+        self.assertEqualArray(expected, got['Y'], decimal=4)
+
+    def test_onnx_lr_only(self):
+        X, y = make_regression(n_features=2)  # pylint: disable=W0632
+        lr = LinearRegression()
+        lr.fit(X, y)
+        X32 = X.astype(numpy.float32)
+
+        last = OnnxSubEstimator(
+            lr, 'X', op_version=max_supported_opset(),
+            initial_types=X32[:1], output_names=['Y'])
+        onx = last.to_onnx(numpy.float32, numpy.float32, verbose=0)
+
+        oinf = OnnxInference(onx)
+        got = oinf.run({'X': X32})
+        expected = lr.predict(X32)
+        self.assertEqualArray(expected, got['Y'].ravel(), decimal=4)
 
 
 if __name__ == "__main__":
--- mlprodict/npy/xop_convert.py
+++ mlprodict/npy/xop_convert.py
@@ -4,6 +4,7 @@
 
 .. versionadded:: 0.9
 """
+import numpy
 from .xop import OnnxOperator
 
 
@@ -69,16 +70,21 @@ class OnnxSubOnnx(OnnxOperator):
             builder.add_initializer(new_name, init)
 
         # linking inputs
-        for name in inputs:
-            new_name = builder.get_unique_name(name)
-            mapped_names[name] = new_name
+        for inp, name in zip(self.model.graph.input, inputs):
+            new_name = builder.get_unique_name(inp.name)
+            mapped_names[inp.name] = new_name
             builder.add_node(
                 'Identity', builder.get_unique_name('_sub_' + name),
                 [name], [new_name])
 
         # adding nodes
         for node in self.model.graph.node:
-            new_inputs = [mapped_names[i] for i in node.input]
+            new_inputs = []
+            for i in node.input:
+                if i not in mapped_names:
+                    raise RuntimeError(
+                        "Unable to find input %r in %r." % (i, mapped_names))
+                new_inputs.append(mapped_names[i])
             new_outputs = []
             for o in node.output:
                 new_name = builder.get_unique_name(o)
@@ -87,8 +93,12 @@ class OnnxSubOnnx(OnnxOperator):
 
             atts = {}
             for att in node.attribute:
-                if att.type == 2:
+                if att.type == 2:  # .i
                     value = att.i
+                    atts[att.name] = value
+                    continue
+                if att.type == 6:  # .floats
+                    value = list(att.floats)
                     atts[att.name] = value
                     continue
                 raise NotImplementedError(
@@ -118,7 +128,7 @@ class OnnxSubEstimator(OnnxSubOnnx):
     :param inputs: inputs
     :param op_version: targetted opset
     :param options: to rewrite the options used to convert the model
-    :param input_types: the implementation may be wrong in guessing
+    :param initial_types: the implementation may be wrong in guessing
         the input types of the model, this parameter can be used
         to overwrite them, usually a dictionary
         `{ input_name: numpy array as an example }`
@@ -134,36 +144,110 @@ class OnnxSubEstimator(OnnxSubOnnx):
 
     def __init__(self, model, *inputs, op_version=None,
                  output_names=None, options=None,
-                 input_types=None, **kwargs):
+                 initial_types=None, **kwargs):
         if model is None:
             raise ValueError("Model cannot be None.")
+        onx = OnnxSubEstimator._to_onnx(
+            model, inputs, op_version=op_version, options=options,
+            initial_types=initial_types, **kwargs)
         OnnxSubOnnx.__init__(
-            self, *inputs, op_version=op_version,
-            output_names=output_names, **kwargs)
-        self.model = model
+            self, onx, *inputs, output_names=output_names)
+        self.ml_model = model
         self.options = options
-        self.input_types = input_types
+        self.initial_types = initial_types
+        self.op_version = op_version
 
     def __repr__(self):
         "usual"
         atts = {}
         for att in ['op_version', 'output_names', 'options',
-                    'input_types']:
+                    'initial_types']:
             value = getattr(self, att, None)
             if value is not None:
                 atts[att] = value
         atts.update(self.kwargs)
-        msg = ", ".join("%s=%r" for k, v in atts.items())
+        msg = ", ".join("%s=%r" % (k, v) for k, v in atts.items())
         if len(atts) > 0:
-            msg += ", "
+            msg = ", " + msg
         return "%s(%r%s)" % (
-            self.__class__.__name__, self.model, msg)
+            self.__class__.__name__, self.ml_model, msg)
 
-    def add_to(self, builder):
+    @staticmethod
+    def _to_onnx(model, inputs, op_version=None, options=None,
+                 initial_types=None, **kwargs):
         """
-        Adds to graph builder.
+        Converts a model into ONNX and inserts it into an ONNX graph.
 
-        :param builder: instance of @see cl _GraphBuilder,
-            it must have a method `add_node`
+        :param model: a trained machine learned model
+        :param inputs: inputs
+        :param op_version: opset versions or None to use the latest one
+        :param options: options to change the behaviour of the converter
+        :param kwargs: additional parameters such as black listed or while listed
+            operators
+        :return: ONNX model
+
+        The method currently supports models trained with
+        :epkg:`scikit-learn`, :epkg:`xgboost`, :epkg`:lightgbm`.
         """
-        raise NotImplementedError()
+        from sklearn.base import BaseEstimator
+
+        if isinstance(model, BaseEstimator):
+            return OnnxSubEstimator._to_onnx_sklearn(
+                model, inputs, op_version=op_version, options=options,
+                initial_types=initial_types, **kwargs)
+        raise RuntimeError(
+            "Unable to convert into ONNX model type %r." % type(model))
+
+    @staticmethod
+    def _to_onnx_sklearn(model, inputs, op_version=None, options=None,
+                         initial_types=None, **kwargs):
+        """
+        Converts a :epkg:`scikit-learn` model into ONNX
+        and inserts it into an ONNX graph. The library relies on
+        function @see fn to_onnx and library :epkg:`skearn-onnx`.
+
+        :param model: a trained machine learned model
+        :param inputs: inputs
+        :param op_version: opset versions or None to use the latest one
+        :param initial_types: if None, the input types are guessed from the
+            inputs. The function converts into ONNX the previous
+            node of the graph and tries to infer the initial_types
+            with the little informations it has. It may not work.
+            It is recommended to specify this parameter.
+        :param options: options to change the behaviour of the converter
+        :param kwargs: additional parameters such as black listed or while listed
+            operators
+        :return: ONNX model
+
+        Default options is `{'zipmap': False}` for a classifier.
+        """
+        from ..onnx_conv.convert import to_onnx
+        if options is None:
+            from sklearn.base import ClassifierMixin
+            if isinstance(model, ClassifierMixin):
+                options = {'zipmap': False}
+        if initial_types is None:
+            # Let's to infer them from previous nodes.
+            raise NotImplementedError(
+                "initial_types is None and the method cannot guess the "
+                "initial_types of the model.")
+
+        if isinstance(initial_types, numpy.ndarray):
+            if len(inputs) != 1:
+                raise RuntimeError(
+                    "The model has %s inputs but only %d input are "
+                    "described in 'initial_types'." % (
+                        len(inputs), 1))
+            X = initial_types
+            initial_types = None
+        elif len(inputs) != len(initial_types):
+            raise RuntimeError(
+                "The model has %s inputs but only %d input are "
+                "described in 'initial_types'." % (
+                    len(inputs), len(initial_types)))
+        else:
+            X = None
+
+        onx = to_onnx(model, X, initial_types=initial_types, options=options,
+                      rewrite_ops=True, target_opset=op_version, **kwargs)
+        return onx
