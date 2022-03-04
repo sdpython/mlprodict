ad5e99233f0ed5b289ca736bf303238cbbbf81a9
xadupre <xavier.dupre@gmail.com>
2022-03-04 20:35:51 +0100

    Adds support for Expand in python runtime

---
+++ mlprodict/onnxrt/ops_cpu/op_expand.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+# pylint: disable=E0203,E1101,C0111
+"""
+@file
+@brief Runtime operator.
+"""
+import numpy
+from onnx.defs import onnx_opset_version
+from ._op import OpRun
+from ..shape_object import ShapeObject
+
+
+def common_reference_implementation(data, shape):
+    ones = numpy.ones(shape)
+    return data * ones
+
+
+class CommonExpand(OpRun):
+
+    def __init__(self, onnx_node, desc=None, expected_attributes=None, **options):
+        OpRun.__init__(
+            self, onnx_node, desc=desc,
+            expected_attributes=expected_attributes, **options)
+
+    def _run(self, data, shape):  # pylint: disable=W0221
+        return (common_reference_implementation(data, shape), )
+
+    def _infer_shapes(self, data, shape):  # pylint: disable=W0221
+        return (ShapeObject(None, dtype=data.dtype), )
+
+    def _infer_types(self, data, shape):  # pylint: disable=W0221
+        return (data, )
+
+    def _infer_sizes(self, *args, **kwargs):
+        res = self.run(*args, **kwargs)
+        return (dict(temp=0), ) + res
+
+
+class Expand_13(CommonExpand):
+
+    def __init__(self, onnx_node, desc=None, **options):
+        CommonExpand.__init__(
+            self, onnx_node, desc=desc, **options)
+
+
+Expand = Expand_13
