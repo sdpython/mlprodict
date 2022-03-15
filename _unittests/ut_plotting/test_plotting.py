1be9eed4539e989864044a257f5a44d18896e17b
xadupre <xadupre@microsoft.com>
2022-03-15 13:04:55 +0100

    fix unit test

--- _unittests/ut_plotting/test_plotting.py
+++ _unittests/ut_plotting/test_plotting.py
@@ -20,10 +20,15 @@ class TestPlotBenchScatter(ExtTestCase):
         data = {(1, 1): 0.1, (10, 1): 1, (1, 10): 2,
                 (10, 10): 100, (100, 1): 100, (100, 10): 1000}
         import matplotlib
-        import matplotlib.pyplot as plt
         if __name__ != "__main__":
             back = matplotlib.get_backend()
             matplotlib.use('Agg')
+        try:
+            import matplotlib.pyplot as plt
+        except Exception as e:
+            raise AssertionError(
+                "Failure due to %r (platform=%r, __name__=%r)." % (
+                    e, platform.platform(), __name__)) from e
         fig, ax = plt.subplots(1, 2, figsize=(10, 4))
         plot_benchmark_metrics(data, ax=ax[0], cbar_kw={'shrink': 0.6})
         plot_benchmark_metrics(data, ax=ax[1], transpose=True, xlabel='X', ylabel='Y',
