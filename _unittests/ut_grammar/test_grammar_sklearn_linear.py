59942a1ab42832ba25b42eebace65b3fb262e51d
xavier dupré <xavier.dupre@gmail.com>
2022-03-27 12:58:00 +0200

    remove one file

--- _unittests/ut_grammar/test_grammar_sklearn_linear.py
+++ _unittests/ut_grammar/test_grammar_sklearn_linear.py
@@ -10,6 +10,7 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.datasets import load_iris
 from mlprodict.testing import iris_data, check_model_representation
 from mlprodict.grammar.grammar_sklearn import sklearn2graph, identify_interpreter
+from mlprodict.grammar.grammar_sklearn.grammar.exc import Float32InfError
 from mlprodict.grammar.cc import compile_c_function
 from mlprodict.grammar.cc.c_compilation import CompilationError
 
@@ -136,6 +137,21 @@ class TestGrammarSklearnLinear(ExtTestCase):
             self.assertIn("value is not a numpy.array but", str(e))
             return
         self.assertGreater(len(rows), 2)
+
+    def test_sklearn_train_lr_into_c(self):
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.datasets import load_iris
+        iris = load_iris()
+        X = iris.data[:, :2]
+        y = iris.target
+        y[y == 2] = 1
+        lr = LogisticRegression()
+        lr.fit(X, y)
+
+        # We replace by double too big for floats.
+        lr.coef_ = numpy.array([[2.45, -3e250]])
+        self.assertRaise(lambda: sklearn2graph(
+            lr, output_names=['Prediction', 'Score']), Float32InfError)
 
 
 if __name__ == "__main__":
