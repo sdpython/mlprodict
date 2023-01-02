120e239cd2cd7ac2d49d4c7bf746b3e71094f67f
xavier dupré <xavier.dupre@gmail.com>
2023-01-02 13:45:53 +0100

    Update xop_auto.py

--- mlprodict/npy/xop_auto.py
+++ mlprodict/npy/xop_auto.py
@@ -494,7 +494,7 @@ def get_onnx_example(op_name):
         try:
             mod = importlib.import_module(m)
             module = m
-        except ImportError:
+        except (AttributeError, ImportError):
             continue
     if module is None:
         # Unable to find an example for 'op_name'.
